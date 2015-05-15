9c12bbc9d0c0d6d11a04e2ef6fecf20ca39a2941
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2015-05-15 16:18:41 +0300

    Fixed small bug

--- cqs/src/main/java/org/testingzone/dbo/doctor/query/DoctorSummaryInfoWrapper.java
+++ cqs/src/main/java/org/testingzone/dbo/doctor/query/DoctorSummaryInfoWrapper.java
@@ -3,6 +3,7 @@ package org.testingzone.dbo.doctor.query;
 import com.mysema.query.annotations.QueryProjection;
 import org.testingzone.dbo.base.AbstractInfoWrapper;
 import org.testingzone.dbo.base.BinaryKey;
+import org.testingzone.dbo.base.SafeBinaryKey;
 import org.testingzone.vo.doctor.query.DoctorSummaryInfo;
 
 public class DoctorSummaryInfoWrapper extends AbstractInfoWrapper<DoctorSummaryInfo> {
@@ -11,7 +12,7 @@ public class DoctorSummaryInfoWrapper extends AbstractInfoWrapper<DoctorSummaryI
 
     @QueryProjection
     public DoctorSummaryInfoWrapper(BinaryKey doctorId, String fullName, String billingNumber, String practiceName, String speciality) {
-        this.doctorSummaryInfo = new DoctorSummaryInfo(doctorId.toString(), fullName, billingNumber, practiceName, speciality);
+        this.doctorSummaryInfo = new DoctorSummaryInfo(new SafeBinaryKey(doctorId).hex(), fullName, billingNumber, practiceName, speciality);
     }
 
     @Override
