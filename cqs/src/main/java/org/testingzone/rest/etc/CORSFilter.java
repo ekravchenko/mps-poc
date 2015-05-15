217e4ba7c42dd908ca7afa3b599fc51e31bd24dd
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2015-05-15 16:06:46 +0300

    Added CorsFilter

---
+++ cqs/src/main/java/org/testingzone/rest/etc/CORSFilter.java
@@ -0,0 +1,27 @@
+package org.testingzone.rest.etc;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@SuppressWarnings("UnusedDeclaration")
+@Component
+public class CORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,x-http-method-override,content-type");
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+}
