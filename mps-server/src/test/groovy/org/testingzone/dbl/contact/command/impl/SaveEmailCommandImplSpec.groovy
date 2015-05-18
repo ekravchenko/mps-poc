199d480aa28c458ba1ce95ca5953be257c5f70fe
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2015-05-19 00:56:59 +0300

    Added tests that fail build

--- mps-server/src/test/groovy/org/testingzone/dbl/contact/command/impl/SaveEmailCommandImplSpec.groovy
+++ mps-server/src/test/groovy/org/testingzone/dbl/contact/command/impl/SaveEmailCommandImplSpec.groovy
@@ -3,22 +3,78 @@ package org.testingzone.dbl.contact.command.impl
 import org.springframework.beans.factory.annotation.Autowired
 import org.testingzone.dbl.BaseSpecification
 import org.testingzone.dbl.contact.command.SaveEmailCommand
+import org.testingzone.dbl.contact.repository.ContactRepository
+import org.testingzone.dbl.contact.repository.EmailRepository
+import org.testingzone.dbo.base.BinaryKey
 import org.testingzone.dbo.base.SafeBinaryKey
-import spock.lang.Ignore
+import org.testingzone.dbo.contact.Contact
+import org.testingzone.dbo.contact.ContactEmail
 
 class SaveEmailCommandImplSpec extends BaseSpecification {
 
     @Autowired
     private SaveEmailCommand saveEmailCommand;
+    @Autowired
+    private EmailRepository emailRepository;
+    @Autowired
+    private ContactRepository contactRepository;
 
-    @Ignore
+    BinaryKey contactPK;
+
+    def setup() {
+        Contact contact = new Contact();
+        contact = contactRepository.save(contact);
+        contactPK = contact.getContactPk();
+    }
+
     def "save new email"() {
         given:
-        def email = "test@test.com";
-        def contactPK = new SafeBinaryKey("31").key();
+        String email = "test@test.com";
+
         when:
-        def emailPK = saveEmailCommand.saveEmail(contactPK, email);
+        BinaryKey emailPK = saveEmailCommand.saveEmail(contactPK, email);
         then:
         emailPK != null;
+
+        when:
+        ContactEmail contactEmail = emailRepository.findOne(emailPK);
+        then:
+        contactEmail != null;
+        contactEmail.getEmail() == email;
+    }
+
+    def "save email with null email string"() {
+        given:
+        String email = null;
+
+        when:
+        saveEmailCommand.saveEmail(contactPK, email);
+        then:
+        def e = thrown(NullPointerException)
+        e.message == "Email is null"
+    }
+
+    def "save email with null contact pk"() {
+        given:
+        contactPK = null;
+        String email = "test@test.com";
+
+        when:
+        saveEmailCommand.saveEmail(contactPK, email);
+        then:
+        def e = thrown(NullPointerException)
+        e.message == "Contact PK is null"
+    }
+
+    def "save email with not existing contact pk"() {
+        given:
+        contactPK = new SafeBinaryKey("1111").key();
+        String email = "test@test.com";
+
+        when:
+        saveEmailCommand.saveEmail(contactPK, email);
+        then:
+        def e = thrown(IllegalArgumentException)
+        e.message == "Error finding contact. PK=1111"
     }
 }
