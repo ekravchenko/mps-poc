f3e60f2881bb2a5edd29061df422f9183ddd5291
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2015-05-26 00:29:07 +0300

    Added Spring4Gwt support

--- mps-server/src/main/java/org/testingzone/Application.java
+++ mps-server/src/main/java/org/testingzone/Application.java
@@ -1,10 +1,13 @@
 package org.testingzone;
 
 
+import org.spring4gwt.server.SpringGwtRemoteServiceServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -19,5 +22,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean springGwtServlet() {
+        return new ServletRegistrationBean(new SpringGwtRemoteServiceServlet(), "/services/*");
     }
 }
