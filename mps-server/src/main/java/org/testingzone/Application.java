4da158ebf8789da8981338d572cdeca322eed922
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2015-05-26 20:59:44 +0300

    IDEA is giving error in configs, which is very strange

--- mps-server/src/main/java/org/testingzone/Application.java
+++ mps-server/src/main/java/org/testingzone/Application.java
@@ -1,20 +1,18 @@
 package org.testingzone;
 
 
-import org.spring4gwt.server.SpringGwtRemoteServiceServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.testingzone.config.DataAccessConfig;
+import org.testingzone.web.etc.SpringGwtRemoteServiceServlet;
 
 @Configuration
-@ComponentScan(basePackages = {"org.testingzone"})
 @Import(DataAccessConfig.class)
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
--- mps-server/src/main/java/org/testingzone/config/DataAccessConfig.java
+++ mps-server/src/main/java/org/testingzone/config/DataAccessConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilde
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -16,7 +15,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"org.testingzone.dbl"})
 @EnableTransactionManagement
 public class DataAccessConfig {
 
